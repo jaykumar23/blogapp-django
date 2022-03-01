b8329c4cf6c46c9002e6cb27a7e6f8892fa2a7b6
Jaykumar sankpal <jaykumarsankpal92@gmail.com>
2022-03-01 19:35:47 +0530

    mains file

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -140,8 +140,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'js1910492@gmail.com'
-EMAIL_HOST_PASSWORD = 'oropynusrmiosvsw'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'Blog Team <noreply@johnsmith.com>'
