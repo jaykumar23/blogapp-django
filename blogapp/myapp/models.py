9f2c72be50aca209a44ce827818741948541337d
Jaykumar sankpal <jaykumarsankpal92@gmail.com>
2022-03-03 17:21:50 +0530

    files added with changes

---
+++ blogapp/myapp/models.py
@@ -0,0 +1,21 @@
+from tkinter import CASCADE
+from turtle import title
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date = models.DateField(default=timezone.now) #created-auto_now_add  
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # user delete-> post delete
+    
+    class Meta:
+        db_table = "Post"
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("post-detail", kwargs={"pk": self.pk})
+    
